2ac58897bde6b4e5d67f64e527157bc97272d1d4
Hansu Kim <hansu1125@gmail.com>
2022-09-22 14:55:15 +0900

    Added export, import groupdata function with pickle file

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 from classes import *
 import math
-
+import openpyxl
+import pandas as pd
+import os
+import pickle
 
 # return: vehdict, lanedict, col_info_list
 def readtxt_VehData(fnum, varlist):
@@ -219,21 +222,24 @@ def structure_lane_composite_distance(base_veh, time, ordered_veh_list, structur
     return ordered_veh_numbering
 
 # Select the vehicles that might be related to the collision
-def select_related_vehicles(front_veh_list, front_veh_numbering, upperbound):
+def select_related_vehicles(front_veh_list, front_veh_numbering, compositionbound, distancebound):
+    veh_list = front_veh_list
+    veh_numbering = front_veh_numbering
+
+    for i in range(len(front_veh_numbering) - 1):
+        if front_veh_numbering[i+1] != front_veh_numbering[i] and front_veh_list[i+1][1] - front_veh_list[i][1] > distancebound:
+            veh_list = front_veh_list[:i+1]
+            veh_numbering = front_veh_numbering[:i+1]
+            break
+
     try:
-        ind = front_veh_numbering.index(upperbound + 1)
-        return front_veh_list[:ind], front_veh_numbering[:ind]
+        ind = veh_numbering.index(compositionbound + 1)
+        return veh_list[:ind], veh_numbering[:ind]
     except ValueError:
-        return front_veh_list, front_veh_numbering
+        return veh_list, veh_numbering
 
-# return: time period that should be analyzed
-def getTimeScope(col_time, plt_history):
-    for i in range(len(plt_history) - 1):
-        if plt_history[i][0] < col_time <= plt_history[i + 1][0]:
-            return [plt_history[i][0], col_time]
-
-#
-def generate_group_object(col_veh, veh_list, veh_numbering, vehdict, lanedict, timescope):
+# Generate group objects and return list of group objects
+def generate_group_object(col_veh, veh_list, veh_numbering, vehdict, lanedict, timescope, col_lane):
     groups = []
     col_veh = Group([col_veh])
     groups.append(col_veh)
@@ -260,7 +266,7 @@ def generate_group_object(col_veh, veh_list, veh_numbering, vehdict, lanedict, t
             end_ind = veh.vardict["timeStamp"].index(timescope[1])
             group.timestamp = veh.getValues("timeStamp")[start_ind:end_ind + 1]
             group.lane = veh.getValues("lane")[start_ind:end_ind + 1]
-            group.capacity = [0 for i in range(end_ind - start_ind)]
+            group.avg_occupancy = [0 for i in range(end_ind - start_ind + 1)]
             group.speed = veh.getValues("speed")[start_ind:end_ind + 1]
 
             for i in range(len(group.timestamp)):
@@ -272,6 +278,9 @@ def generate_group_object(col_veh, veh_list, veh_numbering, vehdict, lanedict, t
                     group.distance.append(round(lane_info[veh_index + 1][1] - lane_info[veh_index][1], 2))
                 else:
                     group.distance.append(math.inf)
+
+            group.lane = list(map(lambda x: 0 if x == col_lane.lanenum else 1, veh.getValues("lane")[start_ind:end_ind + 1]))
+
 
         else:
             back_veh = group.vehlist[0]
@@ -288,14 +297,118 @@ def generate_group_object(col_veh, veh_list, veh_numbering, vehdict, lanedict, t
 
                 front_veh_index = lane_vehs.index(front_veh.vehid)
                 back_veh_index = lane_vehs.index(back_veh.vehid)
-                print(lane_info[front_veh_index][1], lane_info[back_veh_index][1])
-                group.capacity.append(round(lane_info[front_veh_index][1] - lane_info[back_veh_index][1], 2))
+                group.avg_occupancy.append(round((lane_info[front_veh_index][1] - lane_info[back_veh_index][1])/len(group.vehlist), 2))
                 if front_veh_index != len(lane_vehs) - 1:
                     group.distance.append(round(lane_info[front_veh_index + 1][1] - lane_info[front_veh_index][1], 2))
                 else:
                     group.distance.append(math.inf)
 
+            group.lane = list(map(lambda x: 0 if x == col_lane.lanenum else 1, front_veh.getValues("lane")[start_ind:end_ind + 1]))
+
     return groups
+
+# Generating excel files
+def write_group_object_to_xlsx(fnum, groups):
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet.cell(row=1, column=1).value = "Vehicle List"
+    for i in range(len(groups)):
+        sheet.cell(row=2, column = i+1).value = ', '.join(veh.vehid for veh in groups[i].vehlist)
+
+    sheet.append([])
+    indexlist = ['timestamp']
+    for i in range(len(groups)):
+        indexlist += ['group' + str(i) + '_speed', 'group' + str(i) + '_distance', 'group' + str(i) + '_lane', 'group' + str(i) + '_avg_occupancy']
+    sheet.append(indexlist)
+
+    for i in range(len(groups[0].timestamp)):
+        current_time_info = [groups[0].timestamp[i]]
+        for group in groups:
+            current_time_info += [group.speed[i], 'inf' if group.distance[i] == math.inf else group.distance[i], group.lane[i], group.avg_occupancy[i]]
+        sheet.append(current_time_info)
+
+    wb.save('groupdata/' + str(fnum) + '_groupData.xlsx')
+
+# Generating excel files that contains group data from initail raw data
+def make_group_xlsx_from_raw_data(filename, compositionbound, distancebound):
+    col_file = pd.read_excel("data/" + filename, sheet_name='Falselog', engine='openpyxl')
+    col_file_num = list(map(lambda x: int(x[0:4]), col_file['file']))
+
+    for filenum in col_file_num[534:]:
+        try:
+            print("filenum: ", filenum)
+            vehdict, lanedict, col_info = readtxt_VehData(filenum, ["timeStamp", "lane", "lanepos", "speed", "accel"])
+            print("Collision Information: ", col_info)
+            if len(col_info) != 0:
+                structure_history = readtxt_InConfigData(filenum)
+
+                # 두 충돌 차량의 위치를 바탕으로 뒤에 있는 차량 찾기
+                first_col = col_info[0]
+                col_time = first_col[0]
+                col_lane = lanedict[first_col[1]]
+                col_veh1 = vehdict[first_col[2]]
+                col_veh2 = vehdict[first_col[3]]
+                col_veh1_pos = col_veh1.getValues("lanepos", col_time)
+                col_veh2_pos = col_veh2.getValues("lanepos", col_time)
+
+                behind_veh = col_veh1
+                if col_veh1_pos > col_veh2_pos:
+                    behind_veh = col_veh2
+
+                front_veh_list = get_front_veh(behind_veh.vehid, col_time, col_lane)
+                front_veh_numbering = structure_lane_composite_distance(behind_veh.vehid, col_time, front_veh_list, structure_history)
+
+                col_related_veh_list, col_related_veh_numbering = select_related_vehicles(front_veh_list, front_veh_numbering, compositionbound, distancebound)
+
+                print(col_related_veh_list)
+                groups = generate_group_object(behind_veh, col_related_veh_list, col_related_veh_numbering, vehdict, lanedict, (round(col_time - 15, 1), col_time), col_lane)
+
+                write_group_object_to_xlsx(filenum, groups)
+
+        except FileNotFoundError:
+            continue
+
+# Read group excel files and generate MTS
+def read_group_xlsx():
+    path = 'groupdata/'
+    dir_list = os.listdir(path)
+
+    mts_dataset = []
+    for file in dir_list:
+        print(file)
+        sheet = openpyxl.load_workbook(path + file, data_only = True)['Sheet']
+        rows = list(sheet.rows)
+
+        group_list = [group for group in list(map(lambda x: x.value, rows[1])) if group != None]
+        group_num = len(group_list)
+
+        mts = []
+        for line in rows[4:]:
+            mts.append([value for value in list(map(lambda x: x.value, line[1:])) if value != None])
+
+        mts_dataset.append(mts)
+
+    with open('groupdata/mtsdata.pickle', 'wb') as f:
+        pickle.dump(mts_dataset, f)
+
+    return mts_dataset
+
+#
+def preprocess(mts_dataset):
+    min_speed = math.inf
+    max_speed = 0
+    min_distance = math.inf
+    max_distance = 0
+    min_occupancy_ratio = math.inf
+    max_occupancy_ratio = 0
+
+
+
+
+# Calculating two MTS with different dimensions
+def calculate_distance_btw_two_MTS(mts1, mts2):
+    return 0
+
 
 # return: Collision type
 def collision_classification(col_veh1, col_veh2, time):
@@ -303,10 +416,11 @@ def collision_classification(col_veh1, col_veh2, time):
         if "flow" in col_veh2:
             return "env-env"
 
-
-# Select relatively important vehicles based on three distance
-#def grouping(structure_history, lane_history, target_veh, time):
-
+# return: time period that should be analyzed
+def getTimeScope(col_time, plt_history):
+    for i in range(len(plt_history) - 1):
+        if plt_history[i][0] < col_time <= plt_history[i + 1][0]:
+            return [plt_history[i][0], col_time]
 
 
 
--- main.py
+++ main.py
@@ -1,49 +1,17 @@
 from functions import *
-import pandas as pd
+import pickle
 
 filename = 'COLLISION_Cases.xlsx'
-col_file = pd.read_excel("data/" + filename, sheet_name='Falselog', engine='openpyxl')
-col_file_num = list(map(lambda x: int(x[0:4]), col_file['file']))
+compositionbound = 2
+distancebound = 200
 
-for filenum in col_file_num[31:32]:
-    try:
-        print("filenum: ", filenum)
-        vehdict, lanedict, col_info = readtxt_VehData(filenum, ["timeStamp", "lane", "lanepos", "speed", "accel"])
-        print("Collision Information: ", col_info)
-        if len(col_info) != 0:
-            structure_history = readtxt_InConfigData(filenum)
+#make_group_xlsx_from_raw_data(filename, compositionbound, distancebound)
 
-            # 두 충돌 차량의 위치를 바탕으로 뒤에 있는 차량 찾기
-            first_col = col_info[0]
-            col_time = first_col[0]
-            col_lane = lanedict[first_col[1]]
-            col_veh1 = vehdict[first_col[2]]
-            col_veh2 = vehdict[first_col[3]]
-            col_veh1_pos = col_veh1.getValues("lanepos", col_time)
-            col_veh2_pos = col_veh2.getValues("lanepos", col_time)
+#MTS_dataset = read_group_xlsx()
 
-            behind_veh = col_veh1
-            if col_veh1_pos > col_veh2_pos:
-                behind_veh = col_veh2
-
-            front_veh_list = get_front_veh(behind_veh.vehid, col_time, col_lane)
-            front_veh_numbering = structure_lane_composite_distance(behind_veh.vehid, col_time, front_veh_list, structure_history)
-
-            upperbound = 2
-            col_related_veh_list, col_related_veh_numbering = select_related_vehicles(front_veh_list, front_veh_numbering, upperbound)
-
-            groups = generate_group_object(behind_veh, col_related_veh_list, col_related_veh_numbering, vehdict, lanedict, (col_time - 10, col_time))
-            groups[1].preprocess(col_lane.lanenum)
-            print(groups[1].lane)
-
-            #timescope = getTimeScope(col_info)
-
-
-
-    except FileNotFoundError:
-        continue
-
-
+with open('groupdata/mtsdata.pickle', 'rb') as f:
+    MTS_dataset = pickle.load(f)
+print(MTS_dataset)
 
 #print(position_distance('veh1.6', 'flow1.2', collision_time, lanedict['1to2_1'].hist))
 
